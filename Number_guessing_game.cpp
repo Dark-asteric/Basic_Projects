0e89fb9dfc4b08c4604e122f3d89a6de77bf5496
Dark-asteric <111156790+Dark-asteric@users.noreply.github.com>
2022-11-03 23:17:56 +0100

    Add files via upload

---
+++ Number_guessing_game.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+int main(){
+	int num,guess,tries=0;
+	srand(time(NULL));
+	num = rand()%100 + 1;
+	
+	cout<<"********** NUMBER Guessing GaMe **********\n\n";
+	cout<<" ________________________________\n";
+	do{
+		cout<<"|Enter number between 1-100 : ";
+		cin>>guess;
+		cout<<"|";
+		tries++;
+		if(guess>num) cout<<"Too high!\n";
+		else if(guess<num) cout<<"Too low!\n";
+		else
+			cout<<"Correct! # of tries : "<<tries<<"\n|\n";
+	}while(num != guess);
+	
+	cout<<"|You Finish the Number guessing game.\n|Congratulation ! you found the number.\n|\n|Thank you.\n";
+	cout<<" ________________________________\n";
+	return 0;
+}
---
+++ Simple_calculator.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+using namespace std;
+
+int main(){
+	char op;
+	long long n1,n2;
+	cout<<"Enter 1st number : ";
+	cin>>n1;
+	cout<<"\nEnter an operation between (+, -, * ,/) : ";
+	cin>>op;
+	switch(op)
+	{
+		case '+':
+			cout<<"\nEnter 2nd number : ";
+			cin>>n2;
+			cout<<"\nSum of two numbers : "<<n1<<" + "<<n2<<" = "<<n1+n2;
+			break;
+		case '-':
+			cout<<"\nEnter 2nd number : ";
+			cin>>n2;
+			cout<<"\nSubtract of two numbers : "<<n1<<" - "<<n2<<" = "<<n1-n2;
+			break;
+		case '*':
+			cout<<"\nEnter 2nd number : ";
+			cin>>n2;
+			cout<<"\nMultiplication of two numbers : "<<n1<<" * "<<n2<<" = "<<n1*n2;
+			break;
+		case '/':
+			cout<<"\nEnter 2nd number : ";
+			cin>>n2;
+			if(n2 != 0)
+				cout<<"\nDivision of two numbers : "<<n1<<" / "<<n2<<" = "<<(double)n1/n2;
+			else
+				cout<<"Syntax error.\n";
+			break;
+	}
+	return 0;
+}
